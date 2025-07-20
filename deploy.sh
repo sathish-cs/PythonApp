56164a256b2203b7d4a2b7b52423efa9de9f7c16
Sathish <83687894+sathish-cs@users.noreply.github.com>
2025-07-20 17:21:20 +0530

    functions to install packages

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,58 @@ NAMESPACE="hello-world"
 CHART_NAME="app"
 RELEASE_NAME="helloworld"
 
+install_docker() {
+  echo "Installing Docker"
+  sudo apt-get update
+  sudo apt-get install -y ca-certificates curl gnupg lsb-release
+  sudo mkdir -p /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
+    https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt-get update
+  sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+  echo "Docker installed"
+}
+
+install_kubectl() {
+  echo "Installing kubectl"
+  curl -LO "https://dl.k8s.io/release/$(curl -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+  chmod +x kubectl
+  sudo mv kubectl /usr/local/bin/
+  echo "kubectl installed."
+}
+
+install_helm() {
+  echo "Installing Helm"
+  curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+  echo "Helm installed."
+}
+
+if ! command -v docker &> /dev/null; then
+  install_docker
+  sudo usermod -aG docker "$USER"
+  echo "Added $USER to docker group"
+else
+  echo "Docker already installed"
+fi
+
+if ! command -v kubectl &> /dev/null; then
+  install_kubectl
+else
+  echo "kubectl already installed"
+fi
+
+if ! command -v helm &> /dev/null; then
+  install_helm
+  echo "Adding stable Helm repo"
+  helm repo add stable https://charts.helm.sh/stable
+  helm repo update
+else
+  echo "Helm already installed"
+fi
+
 # Build & Push Docker Images
 echo " Building  Hello-World Docker images"
 docker build -t $HELLO_IMAGE ./hello-world
